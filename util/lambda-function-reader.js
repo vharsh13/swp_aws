1910e90e183d8c0ce78f51790ee8534fe8e8310f
vharsh13 <harsh.raimp@gmail.com>
2023-06-09 17:12:17 +0530

    Set the structure to read lambda functions.

---
+++ util/lambda-function-reader.js
@@ -0,0 +1,32 @@
+class LambdaFunctionReader{
+    constructor() {
+        this.connectToServer();
+        this.functionReponse = [{
+            name: 'Hello World',
+            _id: 'xyz'
+        }, {
+            name: 'Hello World Lambda',
+            _id: 'abc'
+        }];
+        this.functionNames = ['Hello World', 'Hello World Lambda'];
+    }
+    connectToServer(){
+        console.log('I am trying to connect with AWS server')
+    }
+    getFunction(){
+        console.log('Getting the function names from the AWS Server');
+        this.parseFunctionResponse();
+        this.displayFunctionNames();
+    }
+
+    parseFunctionResponse(){
+        console.log('I am parsing the function names', this.functionReponse);
+    }
+
+    displayFunctionNames(){
+        console.log('After parsing I am displaying the names', this.functionNames);
+    }
+}
+
+const lambdaFunctionReader = new LambdaFunctionReader();
+lambdaFunctionReader.getFunction();
