c4d1782d7c0db505d4ce37e0001d26939f1ed0da
vharsh13 <harsh.raimp@gmail.com>
2023-06-09 16:05:31 +0530

    Lambda function code

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-const fakeFetch = endpoint => {
-    return new Promise((resolve, reject) => {
-        if (endpoint !== "flight-status") {
-            reject("endpoint not supported.");
-        }
-        setTimeout(() => {
-            resolve({
-                departed: false,
-                delayed: true
-            });
-        }, 1_000);
-    });
+// Import the AWS SDK
+const AWS = require('aws-sdk');
+
+// Create an instance of the Lambda service
+const lambda = new AWS.Lambda();
+
+// Function to get the count of Lambda functions
+async function getLambdaFunctionCount() {
+    try {
+        // Call the ListFunctions API to retrieve the list of functions
+        const response = await lambda.listFunctions().promise();
+
+        // Retrieve the count from the response
+        const count = response.Functions.length;
+
+        // Return the count
+        return count;
+    } catch (error) {
+        console.error('Error retrieving Lambda function count:', error);
+        throw error;
+    }
 }
 
-fakeFetch("flight-status")
-    .then((data) => {
-        console.log(data);
+// Example usage
+getLambdaFunctionCount()
+    .then(count => {
+        // Display the count in an alert
+        alert(`Number of Lambda functions: ${count}`);
     })
     .catch(error => {
-        console.error(error);
-    });
-
-fakeFetch("user-details")
-    .then((data) => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error(error);
+        // Handle any errors
+        console.error('Error:', error);
     });
